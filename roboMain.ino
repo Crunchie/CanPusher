882004077865031d51d3470ad78588dca78c7304
Sam McArdle <smcardle@rtc.crownlift.net>
2014-09-18 20:58:09 +1200

    random turning

--- roboMain.ino
+++ roboMain.ino
@@ -3,20 +3,21 @@
 #define SENSOR_LEFT 0
 #define SENSOR_RIGHT 1
 
-#define SEARCH_SPEED 200
+#define SEARCH_SPEED 255
 
 #define SENSOR_THRESHOLD 500
 
 AF_DCMotor motor2(2, MOTOR12_64KHZ); // create motor #2, 64KHz pwm
 AF_DCMotor motor1(1, MOTOR12_64KHZ);
 void setup() {
-  //Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.begin(9600);           // set up Serial library at 9600 bps
   
   motor1.setSpeed(SEARCH_SPEED);     // set the speed to 200/255
   motor1.run(FORWARD);
   
   motor2.setSpeed(SEARCH_SPEED);     // set the speed to 200/255
   motor2.run(FORWARD);
+  randomSeed(analogRead(3));
 }
 
 void loop() 
@@ -30,19 +31,35 @@ void loop()
 
 	boolean off = false;
 
-	if((left > SENSOR_THRESHOLD) || ( right > SENSOR_THRESHOLD))
+        
+        int error;
+	if(right > SENSOR_THRESHOLD)
 	{
 		off = true;
-		motor1.setSpeed(100);
-		motor2.setSpeed(180);
+		motor1.setSpeed(160);
+		motor2.setSpeed(160);
 
-		motor1.run(BACKWARD);
+		motor1.run(FORWARD);
 		motor2.run(BACKWARD);
 
-		delay(1000);
-	} else
+		delay(random(500, 2000));
+	} else if(left > SENSOR_THRESHOLD)
+        {
+                off = true;
+		motor1.setSpeed(160);
+		motor2.setSpeed(160);
+
+		motor1.run(BACKWARD);
+		motor2.run(FORWARD);
+
+		delay(random(400,1700));
+        }
 	{
 		off = false;
+                if(Serial.available()){
+                  error = Serial.read();
+                }
+                Serial.println(error);
 		motor1.setSpeed(SEARCH_SPEED);
 		motor2.setSpeed(SEARCH_SPEED); 
 
