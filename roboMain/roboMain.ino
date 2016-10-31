b87c0cf6f790d606aea234040e54a75b8b755fe3
Sam McArdle <mcardlesam@gmail.com>
2016-10-31 20:18:41 +1300

    New edge logic. TBT

--- roboMain/roboMain.ino
+++ roboMain/roboMain.ino
@@ -1,5 +1,7 @@
 #include "AFMotor.h"
 
+#define DEBUG 1
+
 #define SENSOR_LEFT 0
 #define SENSOR_RIGHT 1
 
@@ -12,6 +14,12 @@ AF_DCMotor right_motor(1, MOTOR12_64KHZ);
 
 AF_DCMotor left_spinner(4, MOTOR34_64KHZ); // create motor #2, 64KHz pwm
 AF_DCMotor right_spinner(3, MOTOR34_64KHZ);
+
+int leftTriggered;
+int rightTriggered;
+
+void doDebug(int v);
+void checkSensors();
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
@@ -29,19 +37,18 @@ void setup() {
 
   
   randomSeed(analogRead(3));
+  leftTriggered = 0;
+  rightTriggered = 0;
 }
 
-void loop() 
+void loop()
 {
 
-  int left = analogRead(SENSOR_LEFT);
-  int right = analogRead(SENSOR_RIGHT);
 
-  Serial.println(left);
-  Serial.println(right);
+  checkSensors();
 
   int error;
-  if(right > SENSOR_THRESHOLD)
+  if(rightTriggered)
   {
     right_motor.setSpeed(100);
     left_motor.setSpeed(50);
@@ -50,8 +57,8 @@ void loop()
     left_motor.run(BACKWARD);
 
     delay(random(100, 900));
-  } 
-  else if(left > SENSOR_THRESHOLD)
+  }
+  else if(leftTriggered)
   {
     right_motor.setSpeed(50);
     left_motor.setSpeed(100);
@@ -66,9 +73,76 @@ void loop()
   }
   //Serial.println(error);
   right_motor.setSpeed(SEARCH_SPEED);
-  left_motor.setSpeed(SEARCH_SPEED); 
+  left_motor.setSpeed(SEARCH_SPEED);
 
   right_motor.run(FORWARD);
   left_motor.run(FORWARD);
   
+}
+
+void checkSensors()
+{
+    int left = analogRead(SENSOR_LEFT);
+    int right = analogRead(SENSOR_RIGHT);
+    
+    doDebug(left);
+    doDebug(right);
+    
+    static int rightCount = 0;
+    static int leftCount = 0;
+  
+    //Right Sensor hysterysis
+    if(right > SENSOR_THRESHOLD)
+    {
+        if(!rightTriggered)
+        {
+            rightCount++;
+            if(rightCount > 5)
+            {
+                rightTriggered = true;
+            }
+        }
+    }
+    else
+    {
+        if(rightTriggered)
+        {
+            rightCount--;
+            if(rightCount == 0)
+            {
+                rightTriggered = false;
+            }
+        }
+    }
+    
+    // Left Sensor
+    if(left > SENSOR_THRESHOLD)
+    {
+        if(!leftTriggered)
+        {
+            leftCount++;
+            if(leftCount > 5)
+            {
+                leftTriggered = true;
+            }
+        }
+    }
+    else
+    {
+        if(leftTriggered)
+        {
+            leftCount--;
+            if(leftCount == 0)
+            {
+                leftTriggered = false;
+            }
+        }
+    }
+}
+
+void doDebug(int v)
+{
+#ifdef DEBUG
+    Serial.println(v);
+#endif
 }
